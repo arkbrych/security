5e781275fb67f35567b15074154ce11ce2b1bb5f
abrych <arkadiusz.brych@external.t-mobile.pl>
2020-03-26 13:40:15 +0100

    Author: Arkadiusz Brych Description: remember me - repair

--- src/main/java/pl/brych/security/security/SecurityConfig.java
+++ src/main/java/pl/brych/security/security/SecurityConfig.java
@@ -6,9 +6,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -22,12 +26,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     // saved password to database as encrypted charSequence
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User userAdmin = new User("admin",
+                getPasswordEncoder().encode("admin123"),
+                true,
+                true,
+                true,
+                true,
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        auth.inMemoryAuthentication().withUser(userAdmin);
         auth.userDetailsService(userDetailsService);
     }
 
@@ -35,13 +47,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/forAdmin").hasRole("ADMIN")
-                .antMatchers("/forUser").hasRole("USER")
+                .antMatchers("/forUser").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/singup").permitAll()
                 .and()
                 .formLogin().loginPage("/login").defaultSuccessUrl("/forUser").permitAll()
                 .and()
-                .logout().logoutSuccessUrl("/forAll")
+                .rememberMe()
                 .and()
-                .rememberMe();
+                .logout().logoutSuccessUrl("/forAll");
+
     }
 }
