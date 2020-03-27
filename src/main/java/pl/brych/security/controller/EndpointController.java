e43b53a477b51d979eda91a09fdbc8c8465dd0f7
abrych <arkadiusz.brych@external.t-mobile.pl>
2020-03-27 13:02:27 +0100

    Author: Arkadiusz Brych Description: https - repair

--- src/main/java/pl/brych/security/controller/EndpointController.java
+++ src/main/java/pl/brych/security/controller/EndpointController.java
@@ -2,12 +2,13 @@ package pl.brych.security.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 @RequiredArgsConstructor
-public class RestController {
+public class EndpointController {
 
     @GetMapping("/forAll")
     public String forAll() {
